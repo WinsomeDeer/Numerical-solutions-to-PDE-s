5144121862ab9b89fa0261f47a1d878e6ee2f589
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2023-04-04 19:37:06 +0100

    Update 1D_wave_eqn.cpp

--- 1D_wave_eqn.cpp
+++ 1D_wave_eqn.cpp
@@ -6,7 +6,7 @@
 int main(){
 
 // Initialise variables.
-    double X = 10, T = 10; 
+    double X = 10, T = 10, C, dx = 1/X, dt = 1/T; 
     double u[11][11];
 
 // Initialise grid to zeros.
@@ -16,10 +16,24 @@ int main(){
         }
     }
 
-// FDM.
-    for(int t = 0; t <= T; t++){
-        
+// Initliase the grid with initial values.
+    for(int x = 0; x <= X-1; x++){
+        u[x][0] = u[x][1] = sqrt(x);
     }
 
+// FDM.
+    for(int t = 1; t <= T-1; t++){
+        for(int x = 1; x <= X-1; x++){
+            u[x][t] = u[x-1][t] + u[x+1][t] - u[x][t-1];
+        }
+    }
+
+// Print the values.
+    for(int t = 0; t <= T; t++){
+        for(int x = 0; x <= X; x++){
+            std::cout << u[x][t] << " ";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
